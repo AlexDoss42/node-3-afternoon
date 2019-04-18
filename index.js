b7daa54a53da0f1743e03ea232d197711a016a2e
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-18 15:55:28 -0600

    Add SQL files

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const massive = require('massive');
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
 
 const app = express();
 
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 massive(CONNECTION_STRING)
-  .then(dbInstance =>{
-    app.set('db', dbInstance);
-    app.listen(SERVER_PORT, () => {
-      console.log(`It's over Anakin! I have the ${SERVER_PORT} port!`)
-    })
+  .then(dbInstance => {
+    app.set("db", dbInstance);
   })
-    .catch((err) => {
-      console.log(`Houston we have a problem`, err)
-    })
+  .catch(err => console.log(`Houston, we have a ${err}`));
 
+app.use(express.json());
+  
+app.listen(SERVER_PORT, () => {
+  console.log(`It's over Anakin! I have the ${SERVER_PORT} port!`)
+});
